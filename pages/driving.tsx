bbf0d920e1568e064fd31a2404713e63d10c96e3
Viet Duc Kieu <viet.duc.kieu@web.de>
2023-09-16 19:14:38 +0200

    test speed on deployment version

--- pages/driving.tsx
+++ pages/driving.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { motion } from "framer-motion";
 import { useRouter } from 'next/router';
 import {
-    Container, Title, Button, Center, Space
+    Container, Text, Button, Center, Space
 } from '@mantine/core'
 import { StatsRing } from '../components/StatsRing';
 import Lottie from "lottie-react";
@@ -31,19 +31,28 @@ const Driving = () => {
                     "label": "Current driving speed",
                     "stats": "45 km/h",
                     "progress": 100,
-                    "color": "teal",
+                    "color": randomStatus,
                     "icon": "car"
                 },
                 {
                     "label": "Speed limit",
                     "stats": "130 km/h",
                     "progress": 100,
-                    "color": "teal",
+                    "color": randomStatus,
                     "icon": "alert"
                 },
             ]} />
             <Container mih={"80vh"}>
                 <Space h={"xl"} />
+                <Center>
+                    {randomStatus == "green" && <Text align="center">Keep the pace, there is no traffic jam ahead</Text>}
+                    {randomStatus == "yellow" && <Text align="center">Slow down, there is a chance of a traffic jam ahead</Text>}
+                    {randomStatus == "red" && <Text align="center">Slow down, there is a traffic jam ahead</Text>}
+                </Center>
+
+                <Space h={"xl"} />
+
+
                 <Center>
                     {randomStatus == "green" && <Lottie animationData={Green} style={style} />}
                     {randomStatus == "yellow" && <Lottie animationData={Yellow} style={style} />}
